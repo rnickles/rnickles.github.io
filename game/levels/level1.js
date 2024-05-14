e0e61b3c823db6ab74591650a853bd4f3a63682e
rnickles <robbynickles@gmail.com>
2024-05-13 17:15:28 -0700

    setup ground

--- game/levels/level1.js
+++ game/levels/level1.js
@@ -1,14 +1,16 @@
 import { Ball } from "../objects/ball.js"
+import { Ground } from "../objects/ground.js"
 
 export function init_level1(module, engine, game_state) {
 
     // create two boxes and a ground
     var boxA = module.Bodies.rectangle(400, 200, 80, 80);
     var boxB = module.Bodies.rectangle(450, 50, 80, 80);
-    var ground = module.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    // var ground = module.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
     // add all of the bodies to the world
-    module.Composite.add(engine.world, [boxA, boxB, ground]);
+    module.Composite.add(engine.world, [boxA, boxB]);
 
+    new Ground(400, 610, 810, 60, engine, module);
     new Ball(450, 50, engine, module);
 }
