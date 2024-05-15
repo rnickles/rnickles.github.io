e6cbe87bb1b703152badf274a547047bf7cd5ae3
rnickles <robbynickles@gmail.com>
2024-05-14 19:25:54 -0700

    cleaned up code

--- game/levels/level1.js
+++ game/levels/level1.js
@@ -2,15 +2,6 @@ import { Ball } from "../objects/ball.js"
 import { Platform, Goal } from "../objects/platform.js"
 
 export function init_level1(module, engine, game_state) {
-
-    // create two boxes and a ground
-    var boxA = module.Bodies.rectangle(400, 200, 80, 80);
-    var boxB = module.Bodies.rectangle(450, 50, 80, 80);
-    // var ground = module.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-    // add all of the bodies to the world
-    module.Composite.add(engine.world, [boxA, boxB]);
-
     new Platform(400, 610, 810, 60, engine, module); //ground
     let ball = new Ball(450, 50, engine, module);
     let goal = new Goal(400, 400, engine, module, ball, game_state);
--- game/levels/level2.js
+++ game/levels/level2.js
@@ -2,15 +2,6 @@ import { Ball } from "../objects/ball.js"
 import { Platform, Goal } from "../objects/platform.js"
 
 export function init_level2(module, engine, game_state) {
-
-    // create two boxes and a ground
-    var boxA = module.Bodies.rectangle(200, 200, 80, 80);
-    var boxB = module.Bodies.rectangle(250, 50, 80, 80);
-    // var ground = module.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-    // add all of the bodies to the world
-    module.Composite.add(engine.world, [boxA, boxB]);
-
     new Platform(400, 610, 810, 60, engine, module); //ground
     let ball = new Ball(250, 50, engine, module);
     let goal = new Goal(200, 400, engine, module, ball, game_state);
