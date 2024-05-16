f163000c0272c64d60d402515287e1bbcde7f6ad
rnickles <robbynickles@gmail.com>
2024-05-15 20:32:42 -0700

    minor tweaks; variable drop point

--- game/levels/level2.js
+++ game/levels/level2.js
@@ -2,8 +2,7 @@ import { Ball } from "../objects/ball.js"
 import { Platform, Goal } from "../objects/platform.js"
 
 export function init_level2(module, engine, game_state) {
-    new Platform(400, 610, 810, 60, engine, module); //ground
     let goal = new Goal(200, 400, engine, module, game_state);
-
+    game_state.drop_coords = [450, 50];
     game_state.goal = goal;
 }
