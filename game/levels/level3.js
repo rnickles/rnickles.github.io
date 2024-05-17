f5723c0f64e90e0364f18949a75298662dbe6515
rnickles <robbynickles@gmail.com>
2024-05-17 12:31:57 -0700

    added some levels

---
+++ game/levels/level3.js
@@ -0,0 +1,8 @@
+import { Goal, Platform } from "../objects/platform.js"
+
+export function init_level3(module, engine, game_state) {
+    let goal = new Goal(200, 800, engine, module, game_state);
+    game_state.drop_coords = [450, 50];
+    game_state.goal = goal;
+    new Platform(89, 630, 350, 630, engine, module);
+}
