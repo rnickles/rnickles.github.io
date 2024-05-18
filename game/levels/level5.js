02a34a09a7d58733ad819ab465f95bd8e7baa4f5
rnickles <robbynickles@gmail.com>
2024-05-18 14:12:18 -0700

    made level5 mobile-friendly

--- game/levels/level5.js
+++ game/levels/level5.js
@@ -4,9 +4,9 @@
 import { Goal, Platform } from "../objects/platform.js"
 
 export function init_level5(engine, game_state) {
-    let goal = new Goal(800, 800, engine, game_state);
+    let goal = new Goal(200, 800, engine, game_state);
     game_state.drop_coords = [450, 50];
     game_state.goal = goal;
-    new Platform(690, 855, 670, 717, engine);
-    new Platform(656, 744, 1012, 634, engine)
+    new Platform(83, 730, 431, 783, engine);
+    new Platform(331, 732, 321, 849, engine)
 }
