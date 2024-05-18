9abb15f8396ddb64666b54cd7ea27b56df459cf1
rnickles <robbynickles@gmail.com>
2024-05-18 13:12:03 -0700

    removed line

--- game/levels/level1.js
+++ game/levels/level1.js
@@ -4,5 +4,4 @@ export function init_level1(engine, game_state) {
     let goal = new Goal(200, 800, engine, game_state);
     game_state.drop_coords = [450, 50];
     game_state.goal = goal;
-    game_state.total_platform_used = 0;
 }
